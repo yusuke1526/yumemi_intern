cdf480641674cd7d9d5f6c08371a7b573dd191f6
上田 祐介 <y_ueda@MacBook-Pro.local>
2022-10-06 18:54:54 +0900

    モデル（テーブル）の追加

--- api/models.py
+++ api/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
 # Create your models here.
+class Table(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Customer(models.Model):
+    table_id = models.ForeignKey(Table, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Order(models.Model):
+    table_id = models.ForeignKey(Table, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class ItemGenre(models.Model):
+    genre_name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.genre_name
+
+class Item(models.Model):
+    genre_id = models.ForeignKey(ItemGenre, on_delete=models.PROTECT)
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class OrderDetail(models.Model):
+    item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    is_delivered = models.BooleanField(default=False)
+    cancelled_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Option(models.Model):
+    option_name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.option_name
+
+class OrderedOption(models.Model):
+    option_id = models.ForeignKey(Option, on_delete=models.CASCADE)
+    order_detail_id = models.ForeignKey(OrderDetail, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class AvailableOption(models.Model):
+    option_id = models.ForeignKey(Option, on_delete=models.PROTECT)
+    item_id = models.ForeignKey(Item, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
